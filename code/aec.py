eaed06d4aba54d8e85acc258ea29d36c4f51d997
Ogulcan Ozer <oo18830@essex.ac.uk>
2019-02-27 19:09:35 +0000

    clustering

--- code/aec.py
+++ code/aec.py
@@ -5,67 +5,94 @@
 ##************************
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+from sklearn.model_selection import train_test_split
+from sklearn import cluster, datasets, metrics
 import pandas as pd
 import numpy as np
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
+from data import data_struct as DS
 ##************************
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','data'))
 #-------------------------------------------------------------------------------
 # Functions
 #-------------------------------------------------------------------------------
+def read_data():
+
+    #Read and seperate the mnist dataset.
+    mnist = input_data.read_data_sets(os.path.join(path,'mnist'))
+    tr = mnist.train.images
+    trl = mnist.train.labels
+    te = mnist.test.images
+    tel = mnist.test.labels
+
+    mnist_data = DS(tr,trl,te,tel)
+
+    #Read and seperate the HAR dataset.
+    tr1 = pd.read_csv(os.path.join(path,'UCI HAR Dataset','train',
+    'X_train.txt'),delim_whitespace=True,error_bad_lines=False, warn_bad_lines=False,
+    header=None)
+    trl1 = pd.read_csv(os.path.join(path,'UCI HAR Dataset','train',
+    'y_train.txt'), error_bad_lines=False, warn_bad_lines=False,header=None)
+    te1 = pd.read_csv(os.path.join(path,'UCI HAR Dataset','test',
+    'X_test.txt'),delim_whitespace=True,error_bad_lines=False, warn_bad_lines=False,
+    header=None)
+    tel1 = pd.read_csv(os.path.join(path,'UCI HAR Dataset','test',
+    'y_test.txt'),error_bad_lines=False, warn_bad_lines=False, header=None)
+    har_data = DS(tr1,trl1.values.T[0],te1,tel1.values.T[0])
+
+    #Read and seperate the pulsar dataset.
+    alldata = pd.read_csv(os.path.join(path,'HTRU2\HTRU_2.csv'),
+    sep=',',error_bad_lines=False, warn_bad_lines=False,header=None)
+    labels = pd.DataFrame(alldata[alldata.columns[8]])
+    alldata.drop(alldata.columns[8], axis = 1, inplace = True)
+    tr2, te2, trl2, tel2 = train_test_split(alldata, labels, test_size=0.20,
+    random_state=42)
+  
+    pulsar_data = DS(tr2,trl2.values.T[0],te2,tel2.values.T[0])
+
+    return mnist_data, har_data, pulsar_data
+
+def cluster_data(data, labels, cluster_size):
+
+    # kmeans clustering 
+    kM=cluster.KMeans(n_clusters=cluster_size)
+    kM.fit(data)
+    kM.labels_
+    hg, cp, v = metrics.homogeneity_completeness_v_measure(labels, kM.labels_)
+    print("Homogenity: " , hg )
+    print("Completeness: " , cp )
+    print("V-measure: " , v )
 
 #-------------------------------------------------------------------------------
 # Main program.
 #-------------------------------------------------------------------------------
 
-#Read and seperate the mnist dataset.
+# Get all the data as a defined struct
+d_mnist, d_har, d_pulsar = read_data()
 
-mnist = input_data.read_data_sets(os.path.join(path,'mnist'), one_hot=True)
-X_train = mnist.train.images
-y_train = mnist.train.labels
-x_test = mnist.test.images
-y_test = mnist.test.labels
-df = pd.DataFrame(x_test)
-print(df.head)
-corr = df.corr()
-sns.heatmap(corr,cmap="YlGnBu")
-plt.show()
+# Print Shapes...
+#
+# for har,
+print(d_har.X_train.shape)
+print(d_har.Y_train.shape)
+# for pulsar,
+print(d_pulsar.X_train.shape)
+print(d_pulsar.Y_train.shape)
+# for mnist.
+print(d_mnist.X_train.shape)
+print(d_mnist.Y_train.shape)
 
+print("Pulsar : ")
+cluster_data(d_pulsar.X_train,d_pulsar.Y_train,8)
 
+print("Har : ")
+cluster_data(d_har.X_train,d_har.Y_train, 6)
 
-print('-----------------------------------------------------------------------')
-
-#Read and seperate the HAR dataset.
-data1 = pd.read_csv(os.path.join(path,'UCI HAR Dataset','train',
-'X_train.txt'),delim_whitespace=True,error_bad_lines=False, warn_bad_lines=False,
-header=None)
-target1 = pd.read_csv(os.path.join(path,'UCI HAR Dataset','train',
-'y_train.txt'),sep=',',error_bad_lines=False, warn_bad_lines=False)
-#data1['labels'] = target1 
-print(data1.head())
-print(data1.describe())
-corr1 = data1.corr()
-sns.heatmap(corr1,cmap="YlGnBu")
-plt.show()
-
-
-print('-----------------------------------------------------------------------')
-
-#Read and seperate the pulsar dataset.
-data2 = pd.read_csv(os.path.join(path,'HTRU2\HTRU_2.csv'),
-sep=',',error_bad_lines=False, warn_bad_lines=False,header=None)
-target2 = pd.DataFrame(data2[data2.columns[8]])
-data2.drop(data2.columns[8], axis = 1, inplace = True)
-data2.rename(columns=lambda x: str(x),inplace = True)
-print(data2.head())
-print(data2.describe())
-corr2 = data2.corr()
-sns.heatmap(corr2,cmap="YlGnBu")
-plt.show()
-
+print("Mnist : ")
+cluster_data(d_mnist.X_train,d_mnist.Y_train, 10)
 
 # l = list(data2)
 # d, axes = plt.subplots(2, 4)
---
+++ code/data.py
@@ -0,0 +1,12 @@
+
+#_______________________________________________________________________________
+# CE888 Project |        data.py       | Ogulcan Ozer. | Feb. 2019 | UNFINISHED.
+#_______________________________________________________________________________
+
+class data_struct:
+    
+    def __init__(self, X_train, Y_train, x_test, y_test):
+        self.X_train = X_train
+        self.Y_train = Y_train
+        self.x_test = x_test
+        self.y_test = y_test
