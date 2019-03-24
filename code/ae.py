02729fec64b223170e7eeea9b9e821135d26d357
Ogulcan Ozer <oo18830@essex.ac.uk>
2019-03-24 16:35:19 +0000

    fix

--- code/ae.py
+++ code/ae.py
@@ -1,11 +1,12 @@
 #_______________________________________________________________________________
 # CE888 Project |        ae.py       | Ogulcan Ozer. | Feb. 2019 | UNFINISHED.
 #_______________________________________________________________________________
-import ae_config as ae
+import ae_config as ae_conf
 import numpy as np
 import tensorflow as tf
 
 class ae:
+
     def __init__(self, features, labels):
         self.features = features
         self.labels = labels
